79dea51b9247d4e8ae6d6d925499e0728f91268e
Marco Pellegrini <mpellegr@c2r5p6.hive.fi>
2024-08-29 16:27:26 +0300

    corrected EXPORT USER=aaa

--- refresh_env.c
+++ refresh_env.c
@@ -63,44 +63,64 @@ char **delete_env_line(char **copy, char *str)
     return (new_copy);
 }
 
+char    **new_env_line(char **copy, char *str)
+{
+    int i;
+    char **new_copy;
+
+    i = 0;
+    while (copy[i])
+    i++;
+    new_copy = (char **)malloc(sizeof(char *) * (i + 2));
+    i = -1;
+    while (copy[++i])
+        new_copy[i] = strdup(copy[i]);
+    if (!strchr(str, '='))
+    {
+        new_copy[i] = (char *)malloc(sizeof(char) * (strlen(str + 2))); //we can use strjoin here
+        strcpy(new_copy[i], str);
+        strcat(new_copy[i], "=");
+    }
+    else
+        new_copy[i] = strdup(str);
+    new_copy[i + 1] = '\0';
+    i = -1;
+    while (copy[++i])
+        free(copy[i]);
+    free(copy);
+    return (new_copy);
+}
+
 char **replace_or_create_env_line(char **copy, char *str)
 {
     int i;
     size_t len;
-    char **new_copy;
+    int lines;
 
     i = 0;
     len = 0;
+    lines = 0;
+    while (copy[lines])
+        lines++;
     while (str[len] && str[len] != '=')
         len++;
-    if (len == strlen(str))
-    {
-        while (copy[i])
-            i++;
-        new_copy = (char **)malloc(sizeof(char *) * (i + 2));
-        i = -1;
-        while (copy[++i])
-            new_copy[i] = strdup(copy[i]);
-        new_copy[i] = (char *)malloc(sizeof(char) * (strlen(str + 2))); //we can use strjoin here
-        strcpy(new_copy[i], str);
-        strcat(new_copy[i], "=");
-        new_copy[i + 1] = '\0';
-        i = -1;
-        while (copy[++i])
-            free(copy[i]);
-        free(copy);
-        return (new_copy);
-    }
-    else
+    if (len != strlen(str))
     {
         while (copy[i] && strncmp(copy[i], str, len) != 0)
             i++;
-        free(copy[i]);
-        copy[i] = strdup(str);
-        return(copy);
+        if (i == lines)
+            return(new_env_line(copy, str));
+        else
+        {
+            free(copy[i]);
+            copy[i] = strdup(str);
+            return(copy);
+        }
     }
+    else
+        return(new_env_line(copy, str));
 }
-/*
+
 int main(int argc, char **argv, char **envp)
 {
     char **copy;
@@ -111,9 +131,13 @@ int main(int argc, char **argv, char **envp)
     copy = copy_env(envp);
     copy = delete_env_line(copy, "PATH");
     copy = replace_or_create_env_line(copy, "USER=toagne");
-    copy = replace_or_create_env_line(copy, "NEWVAR=sflgbn");
+    copy = replace_or_create_env_line(copy, "NEWVAR");
     i = -1;
     while (copy[++i])
         printf("%s\n", copy[i]);
+    i = -1;
+    while (copy[++i])
+        free(copy[i]);
+    free(copy);
 }
-*/
+
