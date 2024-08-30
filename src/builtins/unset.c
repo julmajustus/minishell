8c2ca290c1a5b69c16ff614d2a67b15e6f9f647e
julmajustus <julmajustus@tutanota.com>
2024-08-30 23:21:21 +0300

    Fixed unset function to work correctly, splitted some functionality from delete_env_line to unset function

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: jmakkone <jmakkone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 22:45:45 by jmakkone          #+#    #+#             */
-/*   Updated: 2024/08/30 14:25:25 by jmakkone         ###   ########.fr       */
+/*   Updated: 2024/08/30 20:45:21 by jmakkone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 char	**ft_unset(char **envp, char *str)
 {
-	// maybe need to handle error case if envp is NULL
-	 //Added check for the equalsing just to handle only cases where only VAR name is given
+	int	i;
+
 	if (!str || !*str || ft_strchr(str, '='))
 		return (envp);
-	return (delete_env_line(envp, str));
+	i = -1;
+	while (envp[++i])
+	{
+		if (ft_strncmp(envp[i], str, ft_strlen(str)) == 0 && 
+			envp[i][ft_strlen(str)] == '=')
+			return (delete_env_line(envp, str));
+	}
+	return (envp);
 }
